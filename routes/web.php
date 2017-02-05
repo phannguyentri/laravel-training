15e07958547b85624808ba5fa5d265cc47ee4991
Phan Nguyen Tri <phnguyentri@gmail.com>
2017-02-05 16:25:54 +0700

    linux 1

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@
 | This file is where you may define all of the routes that are handled
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
-|
+|zzzz
 */
 
 Route::get('/', function () {
